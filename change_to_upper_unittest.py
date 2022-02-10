579e599e91fa730a9f721751e05c2210ddbc4f88
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-02-10 15:40:37 +0000

    some tests

--- change_to_upper_unittest.py
+++ change_to_upper_unittest.py
@@ -8,4 +8,18 @@ class TestSum(unittest.TestCase):
   def test_sum_with_2(self):
     self.assertEqual(change_to_upper(["apple", "banana"], 1), ["apple", "BANANA"])
 
+  def test_sum_with_3(self):
+    self.assertEqual(change_to_upper(["apple", "banana", "strawberry", "berry", "pear", "peach"], 5), ["apple", "banana", "strawberry", "berry", "pear", "PEACH"])
+
+  def test_sum_with_4(self):
+    self.assertEqual(change_to_upper(["apple", "banana", "strawberry", "berry", "pear", "peach"], -1), ["APPLE", "BANANA", "STRAWBERRY", "BERRY", "PEAR", "PEACH"])
+
+  def test_sum_with_5(self):
+    self.assertEqual(change_to_upper(["apple", "banana", "strawberry", "berry"], 5), ["APPLE", "BANANA", "STRAWBERRY", "BERRY"])
+
+  def test_sum_with_5(self):
+    self.assertEqual(change_to_upper(["mary", "valerio", "luis", "jonny", "rita"], 3), ["mary", "valerio", "luis", "JONNY","rita"]) 
+
+  
+
 unittest.main()
