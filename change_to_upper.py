1311ed506358c9153a4e5e08c5c5afe2a28c2fc4
valeriocipolla <valeriocipolla92@gmail.com>
2022-02-10 15:55:11 +0000

    changed instructions

--- change_to_upper.py
+++ change_to_upper.py
@@ -1,6 +1,10 @@
-# you are given a list of strings and a number
-# you should return the list with the string at index "number" in uppercase
-# if there is no element in the list witn index "number" you should return the list with every element uppercase 
+# You are given a list called array and a number called index
+# You should return the list with the string at index "index" changed to upper case
+# If there is no element with that index in the list, you should return the list with all strings changed to upper case
+# Examples:
+# change_to_upper(["apple", "banana"], 1)` should return `["apple", "BANANA"]
+# change_to_upper(["apple", "banana"], 2)` should return `["APPLE", "BANANA"]
+# change_to_upper(["apple", "banana"], -1)` should return `["APPLE", "BANANA"]
 
 def change_to_upper(array, index):
   if index >= len(array):
